d23f27bc81542f9c71f7f806c86d8b5b56f8bbf0
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-11 13:22:55 +0900

    chapter4修了

--- 2_HTML-CSS-Webdesign/chapter4/09-practice-base/script.js
+++ 2_HTML-CSS-Webdesign/chapter4/09-practice-base/script.js
@@ -1,4 +1,17 @@
 /*
-- 猫 35%、犬 30%、きりん 20%、その他 15%
-- 円グラフのオプションには 幅100%、高さ300px を指定
+- 猫 35、犬 30、きりん 20、その他 15
+- 円グラフのオプションには 幅100、高さ300px を指定
 */
+
+var pieData = {
+  labels: ['猫', '犬', 'きりん', 'その他'],
+  series: [35, 30, 20, 15]
+  // seriesに含める数値は％ならそのまま入力で良い※10%なら10
+}
+
+var pieOptions = {
+  width: '100%',
+  height: '440px'
+};
+
+new Chartist.Pie('.animal-chart', pieData, pieOptions);
