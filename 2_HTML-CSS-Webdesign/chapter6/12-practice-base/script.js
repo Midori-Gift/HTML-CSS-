d6d2060c23ddcfff7c76a61d238e058dd3026ebf
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-13 16:40:45 +0900

    chapter6修了

--- 2_HTML-CSS-Webdesign/chapter6/12-practice-base/script.js
+++ 2_HTML-CSS-Webdesign/chapter6/12-practice-base/script.js
@@ -1,2 +1,9 @@
 /* ライトボックスで同一ページ内に画像を表示 */
 /* alt属性のテキストを画像の下に表示させる */
+// new LuminousGallery(document.querySelectorAll(".lightbox"));
+
+new LuminousGallery(document.querySelectorAll(".lightbox"), {}, {
+  caption: function(trigger) {
+    return trigger.querySelector('img').getAttribute('alt');
+  }
+});
